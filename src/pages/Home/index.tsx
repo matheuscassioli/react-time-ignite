60b0bc9e8e566071edbb9862a5c27feed15d154c
Matheus Cassioli <matheuscassioli@hotmail.com>
2024-10-23 19:50:32 -0300

    feature(formulario) : manipulando eventosdo formulario  com react-hook-form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,6 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import {
   CountdonwContainer,
   FormContainer,
@@ -8,13 +11,44 @@ import {
   TaskInput,
 } from "./style";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo deve ser no minimo 5 minutos")
+    .max(60, "O ciclo deve ser no maximo 60 minutos"),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      minutesAmount: 0,
+      task: "",
+    },
+  });
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log("data", data);
+    reset();
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <FormContainer action="">
+      <FormContainer onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <div>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput list="task-sugestions" placeholder="Dê um nome para seu projeto" id="task" />
+          <TaskInput
+            list="task-sugestions"
+            placeholder="Dê um nome para seu projeto"
+            id="task"
+            {...register("task")}
+          />
 
           <datalist id="task-sugestions">
             <option value="projeto 1" />
@@ -30,7 +64,8 @@ const Home = () => {
             placeholder="00"
             step="5"
             min="5"
-            max="60"
+            // max="60"
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </div>
@@ -43,7 +78,9 @@ const Home = () => {
           <span>0</span>
         </CountdonwContainer>
 
-        <StarCountDonwButton type="submit">Comecar</StarCountDonwButton>
+        <StarCountDonwButton disabled={isSubmitDisabled} type="submit">
+          Comecar
+        </StarCountDonwButton>
       </FormContainer>
     </HomeContainer>
   );
