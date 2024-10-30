5812495ad97bd27ca5eca7f56cbcbfd7c0043c1d
Matheus Cassioli <matheuscassioli@hotmail.com>
2024-10-30 01:24:53 -0300

    feature(contextos) : renderizando lista + conta de foi concluido ou nao (logica minha)

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,13 +4,31 @@ import { CyclesContext } from "../../contexts/CyclesContext";
 
 const History = () => {
   const { cycles } = useContext(CyclesContext);
+
+  const renderCycleStatus = (task) => {
+    const { startDate, minutesAmount, interruptedDate } = task;
+
+    const startDateFormated = new Date(startDate);
+    const endDateOrInterruptedDate = new Date(interruptedDate);
+
+    const diferencaMs = Math.abs(startDateFormated - endDateOrInterruptedDate);
+
+    const diferencaMinutos = Math.floor(diferencaMs / (1000 * 60));
+
+    const isComplete = diferencaMinutos == minutesAmount;
+
+    if (interruptedDate) {
+      if (isComplete) {
+        return <Status statusColor="green">Concluido</Status>;
+      }
+      return <Status statusColor="red">Cancelado</Status>;
+    }
+    return <Status statusColor="yellow">Em andamento</Status>;
+  };
+
   return (
     <HistoryContainer>
       <h1>Histórico</h1>
- <pre>
-  {JSON.stringify(cycles)}
- </pre>
-
 
       <HistoryList>
         <table>
@@ -24,38 +42,16 @@ const History = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="red">Interrompido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Em andamento</Status>
-              </td>
-            </tr>
+            {Object.keys(cycles).map((line) => {
+              return (
+                <tr key={cycles[line].id}>
+                  <td>{cycles[line].task}</td>
+                  <td>{cycles[line].minutesAmount} minutos</td>
+                  <td>calculo de faz quanto tempo foi criado</td>
+                  <td>{renderCycleStatus(cycles[line])}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </HistoryList>
